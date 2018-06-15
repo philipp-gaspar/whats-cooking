07f427c48a50649a8c2e672e72f06b0736a8d0ad
Philipp Gaspar <philipp.gaspar@gmail.com>
2018-06-15 16:57:03 -0300

    Download dataset from Kaggle

--- data/download.sh
+++ data/download.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+if [ ! -f train.json ]; then
+    echo "Downloading the dataset:"
+    kaggle competitions download -c whats-cooking -p $PWD
+
+    # Unzip the downloaded files
+    echo "Unzip files:"
+    unzip sample_submission.csv.zip
+    unzip test.json.zip
+    unzip train.json.zip
+
+    # Remove zip files
+    rm *.zip
+
+    printf "\nThe dataset is ready for use!\n"
+else
+    echo "The dataset has already been downloaded."
+fi
