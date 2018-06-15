cd13daed1614f7b18c31b3f2b2128e501676b065
Philipp Gaspar <philipp.gaspar@gmail.com>
2018-06-15 14:28:02 -0300

    Update setup file.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='WhatsCooking',
+    version='0.1',
+    packages=['whats_cooking',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+    author='Philipp Gaspar',
+    author_email='philipp.gaspar@cern.ch',
+)
+
